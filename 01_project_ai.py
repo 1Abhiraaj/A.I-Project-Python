c61cc847a910cd79ac5854f86719221e72d546a2
1Abhiraaj <161248204+1Abhiraaj@users.noreply.github.com>
2024-05-18 04:13:05 +0530

    Update 01_project_ai.py

--- 01_project_ai.py
+++ 01_project_ai.py
@@ -16,6 +16,9 @@ engine.setProperty('voice', voices[0].id)
 def speak(audio):# argumrnt provided to speak the camond 
     engine.say(audio)
     engine.runAndWait()
+    
+def say(text):
+    os.system(f'say "{text}"')
 
 def Wishme():
     hour = int(datetime.datetime.now().hour)
@@ -38,17 +41,16 @@ def takeCommand():
         r.energy_threshold = 300# minimum audio energy to consider for recording
         audio = r.listen(source)#  Records a single phrase from ``source`` (an ``AudioSource`` instance) into an ``AudioData`` instance, which it returns.
 
-    try:
-        print("Recognizing....")
-        query = r.recognize_google(audio, language='en-in')#Using google for voice recognition.
-       # query = r.recognize_google(audio, language='hi-in')#Using google for voice recognition.
-
-        print(f"user said: {query}\n")#User query will be printed.
-    except Exception as e:
-        #print(e)
-        print("Say that againg please... ")#Say that again will be printed in case of improper voice 
-        return "None"#User query will be printed.
-    return query
+        try:
+            print("Recognizing....")
+            query = r.recognize_google(audio, language='en-in')#Using google for voice recognition.
+        # query = r.recognize_google(audio, language='hi-in')#Using google for voice recognition.
+            print(f"user said: {query}\n")#User query will be printed.
+        except Exception as e:
+            #print(e)
+            print("Say that againg please... ")#Say that again will be printed in case of improper voice 
+            return "None"#User query will be printed.
+        return query
 
 def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -61,101 +63,75 @@ def sendEmail(to, content):
 
     
 if __name__ == "__main__":
+    print('Welcome to Jarvis A.I')
+    say("Jarvis A.I")
     Wishme()
-    #takeCommand()
-    #while True:
-    if 1:
-        query = takeCommand().lower()#Converting user query into lower case
-        #Logic for executing tasks based on query
-        
-        if 'wikipedia' in query: #if wikipedia found in the query then this block will be executed
-            speak('Searching Wikipedia....')
-            query = query.replace("wikipedia", "  ")
-            results = wikipedia.summary(query, sentences=2)
-            speak("According to wikipedia")
-            print(results)
-            speak(results)
-            
-        elif 'open youtube' in query:
-            webbrowser.open("youtube.com")
-            speak("Opening sir")
-        elif 'open google' in query:
-            webbrowser.open("google.com")
-            speak("Opening sir")
-
-        elif 'open stackoverflow' in query:
-            webbrowser.open("atackoverflow.com")
-            speak("Opening sir")
-
-        elif 'open MDN web' in query:
-            webbrowser.open("developer.mozilla.org")
-            speak("Opening sir")
-
-        
-        elif 'play music' in query:
-            music_dir = 'F:\SONG\A FOLDER'
-            songs = os.listdir(music_dir)
-            #print(songs)
-            # Use random.choice() to select a random song from the list
-            random_song = random.choice(songs)
-            os.startfile(os.path.join(music_dir, random_song))
-            speak("Opening sir")
-        
-        elif 'the time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")
-            speak(f"Sir, the time is {strTime}")
-            
-        elif 'open code' in query:
-            codePath = "C:\\Users\\abhin\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
-            os.startfile(codePath)
-            speak("Opening sir")
-        
-        elif 'send email' in query:
-            try:
-                speak("what should i say?")
-                content = takeCommand()
-                to = "abhishek.mudit2022@vitstudent.ac.in"
-                sendEmail(to, content)
-                speak("Email has been sent!")
-            except Exception as e:
-                print(e)
-                speak("Sorry . I am not able to send this email")
-        
-        elif 'open camera' in query:
-            try:
-                os.system("start microsoft.windows.camera:")  # Command to open the camera on Windows systems
-                speak("Opening camera")
-            except Exception as e:
-                print(e)
-                speak("Sorry, I couldn't open the camera.")
+    takeCommand()
+    while True:
+        if 1:
+            query = takeCommand()#Converting user query into lower case
+            #Logic for executing tasks based on query
+            sites = [["youtube", "https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"], ["google", "https://www.google.com"],]
+            for site in sites:
+                if f"Open {site[0]}".lower() in query.lower():
+                    say(f"Opening {site[0]} sir...")
+                    webbrowser.open(site[1])
+                    
+            if 'wikipedia' in query: #if wikipedia found in the query then this block will be executed
+                speak('Searching Wikipedia....')
+                query = query.replace("wikipedia", "  ")
+                results = wikipedia.summary(query, sentences=2)
+                speak("According to wikipedia")
+                print(results)
+                speak(results)
                 
+            elif 'open stackoverflow' in query:
+                webbrowser.open("atackoverflow.com")
+                speak("Opening sir")
+
+            elif 'open MDN web' in query:
+                webbrowser.open("developer.mozilla.org")
+                speak("Opening sir")
+
             
-        # to be seen    
-        # elif 'open application' in query:
-        #     try:
-        #         app_name = query.split("open application ")[1]  # Extract the application name from the user query
-        #         if "chrome" in app_name:
-        #             os.startfile("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe")  # Example path for Chrome
-        #             speak("Opening Chrome")
-        #         elif "notepad" in app_name:
-        #             os.startfile("C:\\Windows\\System32\\notepad.exe")  # Example path for Notepad
-        #             speak("Opening Notepad")
-        #         # Add more elif conditions for other applications as needed
-        #         else:
-        #             speak("Sorry, I don't have information about that application.")
-        #     except Exception as e:
-        #             print(e)
-        #             speak("Sorry, I couldn't open the application.")
+            elif 'play music' in query:
+                music_dir = 'F:\SONG\A FOLDER'
+                songs = os.listdir(music_dir)
+                #print(songs)
+                # Use random.choice() to select a random song from the list
+                random_song = random.choice(songs)
+                os.startfile(os.path.join(music_dir, random_song))
+                speak("Opening sir")
+            
+            elif 'the time' in query:
+                strTime = datetime.datetime.now().strftime("%H:%M:%S")
+                speak(f"Sir, the time is {strTime}")
+                
+            elif 'open  vs code' in query:
+                codePath = "C:\\Users\\abhin\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+                os.startfile(codePath)
+                speak("Opening sir")
+            
+            elif 'open camera' in query:
+                try:
+                    os.system("start microsoft.windows.camera:")  # Command to open the camera on Windows systems
+                    speak("Opening camera")
+                except Exception as e:
+                    print(e)
+                    speak("Sorry, I couldn't open the camera.")
+        
+                
+                    
+                
+                            
+                            
+                            
+                            
+                    
+
 
                         
                         
                         
                         
                 
-
-
-                    
-                    
-                    
-                    
-            
