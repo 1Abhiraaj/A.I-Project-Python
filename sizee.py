3e1964b4b776b81bebed62fcabcfa5b3d8ae5cc4
1Abhiraaj <161248204+1Abhiraaj@users.noreply.github.com>
2024-05-21 08:33:51 +0530

    Add files via upload

---
+++ sizee.py
@@ -0,0 +1,16 @@
+import pyttsx3
+import speech_recognition as sr
+import pywhatkit as wk
+
+import pyautogui as pi
+import time
+from time import sleep
+
+# time.sleep(3)
+# print(pi.size())
+# print(pi.position())
+
+pi.hotkey("win","prtsc")
+
+
+
